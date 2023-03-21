f3a079b80724a03906e08152de10ef8e9775fde7
ddpshah <divyesh.shah@spit.ac.in>
2023-03-21 13:40:59 +0530

    upgated api

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const email = document.querySelector("#email");
 const password = document.querySelector("#password");
 
 btn.addEventListener("click", async () => {
+    console.log('btn1 clicked');
     let name = email.value;
     let pass = password.value;
 
@@ -13,8 +14,14 @@ btn.addEventListener("click", async () => {
     console.log(name);
     console.log(pass);
 
-    await fetch(`https://mindful-braid-380816.el.r.appspot.com/insert/10/${name}`, {
-        method: "POST",
-        headers: { 'Content-Type': 'application/json' },
-    });
+    const id = 22;
+
+    const res= await fetch(`https://mindful-braid-380816.el.r.appspot.com/insert/${name}/${pass}`);
+    console.log(res.status);
+    console.log(res.statusText);
+    console.log(res.body);
+
+    const data = await res.json();
+
+    console.log(data);
 })
